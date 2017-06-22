f3194d7fdfd9e2e3b49b2777fa0e4794e45e353e
Luis Hernandez <Luis.hernandez2017@gmail.com>
2017-06-22 21:49:59 +0200

    class binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Design and Templating';
+  property1 = false;
+  property2 = true;
+  property3 = true;
+
+  myClasses ={
+    danger: this.property1,
+    safe: this.property2,
+    changed: this.property3
+  }
+  myFunction(count){
+    console.log(count.value.length);
+  }
+  alertStatus(status){
+    if(status == 'danger'){
+      return true;
+    }
+  }
 }
+
+
