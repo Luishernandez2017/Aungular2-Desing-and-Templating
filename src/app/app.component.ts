ee5ea48914ec80ea9f205355d5bc7e2893977aa3
Luis Hernandez <Luis.hernandez2017@gmail.com>
2017-06-23 00:11:53 +0200

    transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,36 @@ import {trigger, state, style, transition, animate} from '@angular/animations'
 
       state('active', style({
         backgroundColor: '#ffcc00'
-      }))
+      })),
+      transition('active <=>inactive', animate('1000ms ease-in')),
+      //transition('inactive => active', animate('500ms ease-in'))
+    ]),
+    trigger('myAnimation2', [
+      state('inactive', style({
+        transform: 'translateX(-305px)'
+      })),
+      state('active', style({
+        transform: 'translateX(0px)'
+      })),
+      transition('inactive => acitve',
+      animate('300ms ease-in')
+
+
+
+      ),
+
+
     ])
   ]
 })
 export class AppComponent {
   title = 'app';
   animationState= 'inactive';
+   animationState2= 'inactive';
 
   animate(){
-  this.animationState =(this.animationState === 'inactive'? 'active': 'inactive');
+  this.animationState = (this.animationState === 'inactive'? 'active': 'inactive');
+  this.animationState2 = (this.animationState2 === 'inactive'? 'active' :'inactive');
 }
 
 }
